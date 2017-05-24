63b96f94ab68cc5a2c090c8efb98585de6a36b24
Ramiro Cadavid <Ramiro Cadavid>
2017-05-24 10:08:42 -0500

    Incluida prueba de RForcecom

--- Script/ARET carga a SF.R
+++ Script/ARET carga a SF.R
@@ -205,7 +205,6 @@ names(a2.vrCreditos)[2] <- "Almacen"
 # Agregar todos los valor creditos
 vr.credito <- full_join(o2.vrCreditos, f2.vrCreditos, by = "CEDULA")
 vr.credito <- full_join(vr.credito, a2.vrCreditos, by = "CEDULA")
-library(tidyr)
 vr.credito <- gather(vr.credito, "Tipo", "vr.credito", Ordinario:Almacen)
 
 
@@ -339,6 +338,59 @@ t.capital <- data.frame(select(o0.vrCupo, CEDULA),
                         capital = rowSums(o0.vrCupo[, 2:6], na.rm = TRUE))
 
 f.totales <- full_join(f.totales, t.capital, by = "CEDULA")
+
+
+# PRUEBA RFORCECOM --------------------------------------------------------
+
+# Salesforce login
+library(RForcecom)
+username <- "admin@andes.org"
+password <- "admgf2017*XQWRiDpPU6NzJC9Cmm185FF2"
+instanceURL <- "https://taroworks-8629.cloudforce.com/"
+apiVersion <- "36.0"
+session <- rforcecom.login(username, password, instanceURL, apiVersion) 
+
+# Crear datos
+ABC__c <- rep(c("a", "b", "c"), 100)
+Numero__c <- round(rnorm(300, mean = 10, sd = 2), digits = 0)
+Name <- c()
+for(i in 1:300) {
+     Name <- c(Name, paste("name", i))
+}
+
+data <- data.frame(Name, Numero__c, ABC__c)
+
+# Cargar datos
+
+## run an insert job into the Account object
+job_info <- rforcecom.createBulkJob(session, 
+                                    operation='insert', 
+                                    object='RForcecom__c')
+
+## split into batch sizes of 200
+batches_info <- rforcecom.createBulkBatch(session, 
+                                          jobId=job_info$id, 
+                                          data, 
+                                          multiBatch = TRUE, 
+                                          batchSize=200)
+
+## check on status of each batch
+batches_status <- lapply(batches_info, 
+                         FUN=function(x){
+                              rforcecom.checkBatchStatus(session, 
+                                                         jobId=x$jobId, 
+                                                         batchId=x$id)
+                         })
+## get details on each batch
+batches_detail <- lapply(batches_info, 
+                         FUN=function(x){
+                              rforcecom.getBatchDetails(session, 
+                                                        jobId=x$jobId, 
+                                                        batchId=x$id)
+                         })
+## close the job
+close_job_info <- rforcecom.closeBulkJob(session, jobId=job_info$id)
+
 
 # Subir datos a Salesforce ------------------------------------------------
 
